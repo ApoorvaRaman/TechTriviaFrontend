e7d461d424686e9b54ccfe89a9ba372b260d51a0
Apoorva Raman <apoorvaramans@gmail.com>
2025-09-27 14:41:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,14 @@ function showTopics() {
   document.getElementById("topic-selection").style.display = "block";
 }
 function startQuiz(topic) {
-  alert("You selected: " + topic + " 🚀 (quiz will load soon)");
+  fetchQuestions(topic);
+}
+async function fetchQuestions(topic) {
+  try {
+    let res = await fetch(`https://techtrivia-backend.herokuapp.com/api/questions?topic=${topic}`);
+    let data = await res.json();
+    console.log("Questions received:", data);
+  } catch (err) {
+    alert("Error fetching questions.");
+  }
 }
